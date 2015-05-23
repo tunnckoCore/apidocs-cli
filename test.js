5e4bea68b910a1639c4f93e982f7f865eda36b71
tunnckoCore <mameto_100@mail.bg>
2015-05-23 17:43:47 +0300

    improve coverage

--- test.js
+++ test.js
@@ -15,21 +15,26 @@ var apidocs = require('./index')
 test('apidocs-cli:', function () {
   test('should generate API docs from the given file:', function (done) {
     apidocs('fixtures/a.js', function (err, docs) {
-      if (err) {
-        return done(err)
-      }
-      test.ok(/### \[\.aaa\]/.test(docs))
+      test.ifError(err, 'should not have error')
+      test.ok(/### \[\.aaa\]/.test(docs), 'should have ### aaa heading')
       done()
     })
   })
 
   test('should generate API docs from a glob of files:', function (done) {
     apidocs('fixtures/*.js', function (err, docs) {
-      if (err) {
-        return done(err)
-      }
-      test.ok(/### \[\.aaa\]/.test(docs))
-      test.ok(/### \[\.ddd\]/.test(docs))
+      test.ifError(err, 'should not have error')
+      test.ok(/### \[\.aaa\]/.test(docs), 'should have ### aaa heading')
+      test.ok(/### \[\.ddd\]/.test(docs), 'should have ### ddd heading')
+      done()
+    })
+  })
+
+  test('should be able to pass options to `helper-apidocs`:', function (done) {
+    apidocs('fixtures/tmp-file.js', {sep: '\n'}, function (err, docs) {
+      test.ifError(err, 'should not have error')
+      test.ok(/### \[\.generate\]/.test(docs), 'should have ### generate heading')
+      test.ok(/### \[\.vinyl\]/.test(docs), 'should have ### vinyl heading')
       done()
     })
   })
