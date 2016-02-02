47aad668e165bf1f1a325e2157f56cbf702d1a45
tunnckoCore <mameto_100@mail.bg>
2016-02-02 14:51:32 +0200

    update deps, add .sync method

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 /*!
  * apidocs-cli <https://github.com/tunnckoCore/apidocs-cli>
  *
- * Copyright (c) 2015 Charlike Mike Reagent <@tunnckoCore> (http://www.tunnckocore.tk)
+ * Copyright (c) 2015-2016 Charlike Mike Reagent <@tunnckoCore> (http://www.tunnckocore.tk)
  * Released under the MIT license.
  */
 
 'use strict'
 
-var apidocs = require('helper-apidocs')
+var apidocs = require('helper-apidocs')()
 
 /**
  * Generate API docs from code comments in the JavaScript
@@ -15,15 +15,14 @@ var apidocs = require('helper-apidocs')
  * with `@api public` are rendered.
  *
  * @name   apidocsCli
- * @param  {Array|String} `<files>` filepaths or glob patterns
+ * @param  {String} `<files>` filepath(s) or glob pattern(s)
  * @param  {Object} `[options]` options to pass to [helper-apidocs][apidocs] or `callback`
  * @param  {Function} `<callback>` node-style callback function `(err, res)`
  * @api public
  */
-
 module.exports = function apidocsCli (files, options, callback) {
-  if (!Array.isArray(files) && typeof files !== 'string') {
-    throw new TypeError('apidocs-cli: expect `files` to be string or array')
+  if (typeof files !== 'string') {
+    throw new TypeError('apidocs-cli: expect `files` to be string')
   }
   callback = typeof options === 'function' ? options : callback
   options = typeof options === 'object' ? options : {}
@@ -32,5 +31,27 @@ module.exports = function apidocsCli (files, options, callback) {
     throw new TypeError('apidocs-cli: expect callback')
   }
 
-  return apidocs(files, options, callback)
+  var res = null
+  try {
+    res = apidocs(files, options)
+  } catch (err) {
+    callback(err)
+    return
+  }
+  callback(null, res)
+}
+
+/**
+ * Synchronous generating API Docs.
+ *
+ * @name   .sync
+ * @param  {String} `<files>` filepath(s) or glob pattern(s)
+ * @param  {Object} `[options]` options to pass to [helper-apidocs][apidocs]
+ * @api public
+ */
+module.exports.sync = function apidocsCliSync (files, options) {
+  if (typeof files !== 'string') {
+    throw new TypeError('apidocsCli.sync: expect `files` to be string')
+  }
+  return apidocs(files, options)
 }
--- test.js
+++ test.js
@@ -12,30 +12,42 @@
 var test = require('assertit')
 var apidocsCli = require('./index')
 
-test('apidocs-cli:', function () {
-  test('should throw TypeError when no arguments', function (done) {
-    function fixture () {
-      apidocsCli()
-    }
+test('should throw TypeError when no arguments (async)', function (done) {
+  function fixture () {
+    apidocsCli()
+  }
 
-    test.throws(fixture, TypeError)
-    test.throws(fixture, /apidocs-cli: expect `files` to be string or array/)
-    done()
-  })
-  test('should throw Error if no callback', function (done) {
-    function fixture () {
-      apidocsCli(['vez.js', 'file/path.js', 'foo/*.css'])
-    }
+  test.throws(fixture, TypeError)
+  test.throws(fixture, /apidocs-cli: expect `files` to be string/)
+  done()
+})
 
-    test.throws(fixture, TypeError)
-    test.throws(fixture, /apidocs-cli: expect callback/)
+test('should throw TypeError if invalid `files` (.sync)', function (done) {
+  function fixture () {
+    apidocsCli.sync()
+  }
+
+  test.throws(fixture, TypeError)
+  test.throws(fixture, /apidocsCli.sync: expect `files` to be string/)
+  done()
+})
+
+test('should throw Error if no callback', function (done) {
+  function fixture () {
+    apidocsCli('vez.js')
+  }
+
+  test.throws(fixture, TypeError)
+  test.throws(fixture, /apidocs-cli: expect callback/)
+  done()
+})
+
+test('should generate API docs from the given file:', function (done) {
+  apidocsCli('./index.js', function (err, res) {
+    test.ifError(err)
+    test.equal(/### \[apidocsCli\]/.test(res), true)
     done()
-  })
-  test('should generate API docs from the given file:', function (done) {
-    apidocsCli('./index.js', function (err, res) {
-      test.ifError(err)
-      test.equal(/### \[\.apidocsCli\]/.test(res), true)
-      done()
-    })
   })
 })
+
+console.log(apidocsCli.sync('./index.js'))
